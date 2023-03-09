9531d30a7c9d3b279a2e5651789c9b4f40d78d46
nicolas prevot <nicolas1.prevot@orange.com>
2023-03-09 15:32:46 +0100

    added base data layout example

---
+++ src/scripts/data.js
@@ -0,0 +1,10 @@
+const Data = Object.freeze({
+    Charset: {
+        AMAROK: 'amarok.png',
+        BETHEROS: 'betheros.png',
+        BRIM: 'brim.png',
+        CARHAL: 'carhal.png',
+        IFRIN: 'ifrin.png',
+        NAKA: 'naka.png'
+    }
+});
