5b359948ff27555bae192ed8ad1a97bf25451f9c
ntrx <kraenash@gmail.com>
2023-03-12 12:01:42 +0100

    tweaked stat.js and added extra doc

--- src/scripts/stat.js
+++ src/scripts/stat.js
@@ -1,6 +1,6 @@
 /**
  * The Stat class is used to define any kind of effect within the game : Rune stats, Skills effects...
- * Two parameters are mandatory: the EFFECT and the VALUE (which must be an array). 
+ * Two parameters are mandatory: the EFFECT and the THEORICAL VALUE (which must be an array). 
  * If size 1, the VALUE is fixed and will always be the same. 
  * If size 2, the VALUE will be random (given that the first element of the array is the minimum, and the second element is the maximum) 
  * each time it is called by the getValue() method.
@@ -10,33 +10,41 @@
 class Stat {
     /**
      * @param {string} effect the Effect given by the Stat object
-     * @param {array} value the value of the Stat object
+     * @param {array} theorical the theorical value of the Stat object
+     * @param {boolean} fixed TRUE means the value will always be the same, FALSE (default) means it will always be random
      * @param {boolean} isPercentage is the value a percentage?
      * @param {boolean} critical is the Stat a critical effect?
      * @param {number} duration 0 = permanent, > 0 = duration in rounds,
      * @param {number} delay 0 = immediate, < 0 = delay in rounds
      * @param {string} type is it a PASSIVE or ACTIVE Stat?
      */
-    constructor(effect, value, isPercentage = false, critical = false, duration = 0, delay = 0, type = Data.StatType.PASSIVE) {
+    constructor(effect, theorical, fixed = false, isPercentage = false, critical = false, duration = 0, delay = 0, type = Data.StatType.PASSIVE) {
         this.effect = effect;
-        this.value = value;
+        this.theorical = theorical;
         this.isPercentage = isPercentage;
         this.critical = critical;
         this.duration = duration;
         this.delay = delay;
         this.type = type;
+        this.value = null;
+
+        if(fixed) {
+            this.value = this.getValue();
+            this.fixed = fixed;
+        }
     }
 
     /**
-     * Returns the Stat's value. If it's a size 2 array, the value is randomly generated between the two bounds.
+     * Returns the Stat's value. If it's a size 2 array, the value is randomly generated between the two bounds. If the Stat has the FIXED attribute set to TRUE, it will always return the same value.
      * @returns {number} the Stat's value
      */
     getValue() {
-        if(this.value.length == 2) {
-            let min = this.value[0];
-            let max = this.value[1];
-            return Math.floor(Math.random() * (max - min + 1)) + min;
+        if(this.fixed) return this.value;
+        if(this.theorical.length == 2) {
+            let min = this.theorical[0];
+            let max = this.theorical[1];
+            return getRandomNumber(min, max);
         }
-        return this.value[0];
+        return this.theorical[0];
     }
 }
