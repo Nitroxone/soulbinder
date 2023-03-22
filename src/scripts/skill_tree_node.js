3e99741550099e70df4993f40fa8283e3416d790
nicolas prevot <nicolas1.prevot@orange.com>
2023-03-22 14:53:00 +0100

    added manipulation functions to skilltreenodes

--- src/scripts/skill_tree_node.js
+++ src/scripts/skill_tree_node.js
@@ -6,10 +6,65 @@ class SkillTreeNode {
         this.type = type
         this.levels = levels;
         this.requirements = requirements
-        this.previous = null;
-        this.next = null;
+        this.previous = [];
+        this.next = [];
         this.rewards = rewards;
 
         this.currentLevel = 0;
+    }
+
+    /**
+     * Adds a level to the Node. Cannot exceed the Node's maximum level.
+     */
+    addLevel() {
+        this.currentLevel = Math.min(++this.currentLevel, this.levels);
+    }
+
+    /**
+     * Adds the provided Node to the Parent Nodes of this Node.
+     * @param {SkillTreeNode} next the Parent Node to add
+     */
+    addPrevious(previous) {
+        this.previous.push(previous);
+    }
+
+    /**
+     * Adds the provided Node to the Children Nodes of this Node.
+     * @param {SkillTreeNode} next the Child Node to add
+     */
+    addNext(next) {
+        this.next.push(next);
+    }
+
+    /**
+     * Returns the requirements to unlock the Node's next level.
+     * @returns {array} the requirements to the Node's next level
+     */
+    getNextRequirements() {
+        return this.requirements[this.currentLevel+1];
+    }
+
+    /**
+     * Returns the required level to unlock the Node's next level.
+     * @returns {number} the required level to unlock the Node's next level
+     */
+    getNextRequiredLevel() {
+        return this.getNextRequirements()[0];
+    }
+
+    /**
+     * Returns the required amount of skill points to unlock the Node's next level.
+     * @returns {number} the required skill points amount to unlock the Node's next level
+     */
+    getNextRequiredSkillPoints() {
+        return this.getNextRequirements()[1];
+    }
+
+    /**
+     * Returns the rewards at the next Node level.
+     * @returns {array} the rewards at the next Node level
+     */
+    getNextRewards() {
+        return this.rewards[this.currentLevel+1];
     }
 }
