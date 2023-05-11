07141eb8dcfd9f7452282a239918331da6092940
nicolas prevot <nicolas1.prevot@orange.com>
2023-05-11 12:10:25 +0200

    fixed speech

--- src/scripts/speech.js
+++ src/scripts/speech.js
@@ -1,14 +1,27 @@
 let Speech = {
     Dungeon: {
-        Entrance: {
-            Dense: {
-                JUNGLE: ""
-            },
-            Cave: {
-                JUNGLE: ""
-            },
-            Village: {
-                JUNGLE: ""
+        Dense: {
+            Entrance: {
+                JUNGLE: [
+                    "Quote 1",
+                    "Quote 2"
+                ]
+            }
+        },
+        Cave: {
+            Entrance: {
+                JUNGLE: [
+                    "Quote 1",
+                    "Quote 2"
+                ]
+            }
+        },
+        Village: {
+            Entrance: {
+                JUNGLE: [
+                    "Quote 1",
+                    "Quote 2"
+                ]
             }
         }
     }
