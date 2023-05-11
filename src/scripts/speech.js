f72002b0903b142a2bbe495c8540c3306d234d13
wensaqt <116151112+wensaqt@users.noreply.github.com>
2023-05-11 14:41:38 +0200

    changed dungeon quotes

--- src/scripts/speech.js
+++ src/scripts/speech.js
@@ -14,21 +14,33 @@ let Speech = {
 
                     "After a long journey, your striders land at the entrance of a cave. The vegetation is dense outside, and it is impossible to see what is inside. Who knows what you might stumble upon?",
 
-                    "The exploration was difficult, but you and your striders eventually got where you wanted to go. Before you, a gaping cave from which emanates putrid fumes. The cave seems to spread out in an intricate network of guts, like intestines ready to slowly digest you. Do you dare to venture into it?"
+                    "The exploration was difficult, but you and your striders eventually got where you wanted to go. Before you, a gaping cave from which emanates putrid fumes. Do you dare to venture into it?"
 
                 ],
             },
 
             Regular: {
-                JUNGLE_BIG_ROOM: [
+                JUNGLE_ROOM: [
 
-                    "You enter a huge room, with limestone stalactites on the ceiling; sharp as fangs and decayed by mold. On the walls, a strange mushroom that lights up when you pass by... Useful to find your way.",
+                    "A small room opens to you. The walls are covered with moss, and be careful where you put your feet: the ambient humidity has made the floor slippery. It would be very unfortunate to have arrived here just to die stupidly. But the eons seem to trust you; you're not an idiot... Are you?",
+
+                    "You enter a vast chamber adorned with limestone stalactites, sharp as fangs and tinged with mold. The walls are embellished with luminescent mushrooms that softly glow as you pass by, providing a helpful source of light to navigate through the room. The eerie combination of stalactites and glowing fungi creates an otherworldly atmosphere, casting mysterious shadows and illuminating hidden details. The chamber feels ancient and untouched, hinting at a forgotten history waiting to be unveiled.",
 
                 ],
-                JUNGLE_SMALL_ROOM: [
+
+                JUNGLE_ACTION: [
+
+                    "The cave seems to spread out in an intricate network of guts, like intestines ready to slowly digest you. But you still have the option of staying on the level you are on to explore the surrounding rooms. Which way do you want to go?"
 
                 ],
-                JUNGLE_TUNNEL: [
+
+                JUNGLE_BRIDGE: [
+
+                    "You enter a dark and narrow tunnel, a nauseating smell assaults your nose. Once again, the progression is difficult as the path is sinuous and strewn with heavy stones.",
+
+                ],
+
+                JUNGLE_HOSTILE_ENCOUNTER: [
 
                 ]
             }
