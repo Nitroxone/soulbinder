492a0873f9247d9313286053748ec5f5171a478b
nicolas prevot <nicolas1.prevot@orange.com>
2023-05-11 14:43:14 +0200

    reorganized speech.js structure

--- src/scripts/speech.js
+++ src/scripts/speech.js
@@ -20,29 +20,28 @@ let Speech = {
             },
 
             Regular: {
-                JUNGLE_ROOM: [
+                Jungle: {
+                    ROOM: [
 
-                    "A small room opens to you. The walls are covered with moss, and be careful where you put your feet: the ambient humidity has made the floor slippery. It would be very unfortunate to have arrived here just to die stupidly. But the eons seem to trust you; you're not an idiot... Are you?",
+                        "A small room opens to you. The walls are covered with moss, and be careful where you put your feet: the ambient humidity has made the floor slippery. It would be very unfortunate to have arrived here just to die stupidly. But the eons seem to trust you; you're not an idiot... Are you?",
+    
+                        "You enter a vast chamber adorned with limestone stalactites, sharp as fangs and tinged with mold. The walls are embellished with luminescent mushrooms that softly glow as you pass by, providing a helpful source of light to navigate through the room. The eerie combination of stalactites and glowing fungi creates an otherworldly atmosphere, casting mysterious shadows and illuminating hidden details. The chamber feels ancient and untouched, hinting at a forgotten history waiting to be unveiled.",
+    
+                    ],
+                    ACTION: [
 
-                    "You enter a vast chamber adorned with limestone stalactites, sharp as fangs and tinged with mold. The walls are embellished with luminescent mushrooms that softly glow as you pass by, providing a helpful source of light to navigate through the room. The eerie combination of stalactites and glowing fungi creates an otherworldly atmosphere, casting mysterious shadows and illuminating hidden details. The chamber feels ancient and untouched, hinting at a forgotten history waiting to be unveiled.",
+                        "The cave seems to spread out in an intricate network of guts, like intestines ready to slowly digest you. But you still have the option of staying on the level you are on to explore the surrounding rooms. Which way do you want to go?"
+    
+                    ],
+                    BRIDGE: [
 
-                ],
+                        "You enter a dark and narrow tunnel, a nauseating smell assaults your nose. Once again, the progression is difficult as the path is sinuous and strewn with heavy stones.",
+    
+                    ],
+                    HOSTILE_ENCOUNTER: [
 
-                JUNGLE_ACTION: [
-
-                    "The cave seems to spread out in an intricate network of guts, like intestines ready to slowly digest you. But you still have the option of staying on the level you are on to explore the surrounding rooms. Which way do you want to go?"
-
-                ],
-
-                JUNGLE_BRIDGE: [
-
-                    "You enter a dark and narrow tunnel, a nauseating smell assaults your nose. Once again, the progression is difficult as the path is sinuous and strewn with heavy stones.",
-
-                ],
-
-                JUNGLE_HOSTILE_ENCOUNTER: [
-
-                ]
+                    ]
+                },
             }
         },
         Village: {
