85cb3ebb28e7aafd242fdfe04dceaaf665cc0c97
ntrx <kraenash@gmail.com>
2023-05-31 22:22:29 +0200

    added trinket and icons

--- src/scripts/loader.js
+++ src/scripts/loader.js
@@ -830,7 +830,31 @@ const Loader = {
                         theorical: [-3, -5],
                     })
                 ]
-            )
+            ),
+            new Trinket(
+                "Fire Lizard Talisman",
+                "\"Quote\" - The Ashen Warden",
+                29,
+                10,
+                Data.Rarity.EPIC,
+                [
+                    new Stat({
+                        effect: Data.Effect.MODIF_DMG_POISON,
+                        theorical: [4, 7],
+                        isPercentage: true
+                    }),
+                    new Stat({
+                        effect: Data.Effect.MODIF_CRIT_POISON,
+                        theorical: [6, 8],
+                        isPercentage: true
+                    }),
+                    new Stat({
+                        effect: Data.Effect.PROTECTION,
+                        theorical: [-8, -15],
+                        isPercentage: true
+                    })
+                ],
+            ),
         ];
 
         for(const trinket of trinkets) {
