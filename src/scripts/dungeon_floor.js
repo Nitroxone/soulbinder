89347f256da6468c1a4341f900e9c46204c4322c
ntrx <kraenash@gmail.com>
2023-06-03 21:55:08 +0200

    commented

--- src/scripts/dungeon_floor.js
+++ src/scripts/dungeon_floor.js
@@ -2,7 +2,7 @@ class DungeonFloor {
     constructor(props) {
         this.depth = getValueFromObject(props, "depth", 0);
         this.roomsAmount = getValueFromObject(props, "roomsAmount", getRandomNumber(20, 30));
-        this.gridSize = getValueFromObject(props, "gridSize", [60, 40]);
+        this.gridSize = getValueFromObject(props, "gridSize", [40, 20]);
         this.config = getValueFromObject(props, "config", {
             roomTypes: {
                 "boss room": 1,
@@ -21,23 +21,27 @@ class DungeonFloor {
     }
 
     generateRooms() {
-        let reservedCoordinates = [];
-        let coordinates, roomType;
-        let types = [];
+        let reservedCoordinates = []; // Will contain all of the coordinates that have already been generated
+        let coordinates; // Coordinates for each room
+        let types = []; // Contains all of the room types that must be added
 
+        // Add all of this floor's room types (in this.config.roomTypes) to the list of types that will be added
         for(const rt in this.config.roomTypes) {
             const count = this.config.roomTypes[rt];
             for(let i = 0; i < count; i++) {
                 types.push(rt);
             }
         }
+        // Determine the remaining amount of rooms to fill (with empty rooms), and fill them
         let toAdd = this.roomsAmount - types.length;
         for(let i = 0; i < toAdd; i++) {
             types.push(Data.DungeonRoomType.EMPTY);
         }
-        console.log('There are ' + types.length + ' room types for ' + this.roomsAmount + ' rooms.');
         
+        // For each room
         for(let i = 0; i < this.roomsAmount; i++) {
+            // Create coordinates. Regenerate new coordinates if they already exist in the reservedCoordinates array.
+            // First and last room of the dungeon are the Entrance and the Chasm (exit). They're positioned on the first row, middle and last row, middle.
             do {
                 if(i === 0) coordinates = [0, Math.round((this.gridSize[1] - 1) / 2)];
                 else if(i === this.roomsAmount - 1) coordinates = [this.gridSize[0] - 1, Math.round((this.gridSize[1] - 1) / 2)];
@@ -45,6 +49,8 @@ class DungeonFloor {
             } while(reservedCoordinates.includes(coordinates));
             reservedCoordinates.push(coordinates);
 
+            // Create new DungeonRoom with coordinates.
+            // First and last rooms are Entrance and Chasm.
             this.rooms.push(new DungeonRoom({
                 coordinates: coordinates,
                 type: i === 0 ? Data.DungeonRoomType.ENTRANCE : i === this.roomsAmount-1 ? Data.DungeonRoomType.CHASM : types[i]
