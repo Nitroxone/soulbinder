1a812674fe7916fa379470261aa9aca055d884cd
nicolas prevot <nicolas1.prevot@orange.com>
2023-06-14 16:34:16 +0200

    small fix

--- src/scripts/enemy_behavior.js
+++ src/scripts/enemy_behavior.js
@@ -2,4 +2,10 @@ class EnemyBehavior {
     constructor(props) {
         this.actions = getValueFromObject(props, "actions", []);
     }
+
+    build() {
+        this.actions.forEach(ac => {
+            ac.build();
+        });
+    }
 }
