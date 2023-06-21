5bc49346e76af4eab6795f6692db7bbe32bf1aea
nicolas prevot <nicolas1.prevot@orange.com>
2023-06-21 13:47:15 +0200

    added alchemy doc and fixed clearingredients method

--- src/scripts/alchemy.js
+++ src/scripts/alchemy.js
@@ -8,20 +8,37 @@ class Alchemy {
         this.effects = [];
     }
 
+    /**
+     * Adds the AlchemicalIngredient item whose ID is stored in the provided Event's DataTransfer to the Alchemy selected ingredients.
+     * It will be added to the provided index in the array.
+     * @param {Event} event the Event that contains the data
+     * @param {number} index where to add the ingredient
+     */
     addIngredient(event, index) {
         const ingredient = game.player.inventory.getItemFromId(Data.ItemType.RESOURCE, event.dataTransfer.getData('ingredient'));
         this.ingredients[index] = ingredient;
         document.querySelectorAll('.alchIngredient')[index].innerHTML = getAlchemyIngredient(this.ingredients[index]);
         generateAlchemyIngredientEvents(this.ingredients[index]);
     }
+
+    /**
+     * Removes the AlchemicalIngredient at the provided position in the array.
+     * @param {number} index which ingredient to remove
+     */
     removeIngredient(index) {
         this.ingredients[index].unlink();
         this.ingredients[index] = null;
         document.querySelectorAll('.alchIngredient')[index].innerHTML = getAlchemyIngredient(this.ingredients[index]);
     }
+
+    /**
+     * Removes all of the AlchemicalIngredients.
+     */
     clearIngredients() {
-        this.ingredients = [];
-        document.querySelectorAll('.alchIngredient')[index].innerHTML = getAlchemyIngredient(this.ingredients[index]);
+        for(let i = 0; i < this.ingredients.length; i++) {
+            if(this.ingredients[i]) this.removeIngredient(i);
+        }
+        getAlchemyPotionPreviewEffects(true);
     }
 
     addEffect(eff) {
