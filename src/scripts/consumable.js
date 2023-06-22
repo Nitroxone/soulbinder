c08f6cfc6460ca7b5a2dfc259f0abdd5d740bc87
nicolas prevot <nicolas1.prevot@orange.com>
2023-06-22 15:46:12 +0200

    remove useless consumable property

--- src/scripts/consumable.js
+++ src/scripts/consumable.js
@@ -5,13 +5,11 @@ class Consumable extends Item {
      * @param {number} icon The Consumable's icon
      * @param {number} price The Consumable's price
      * @param {string} rarity The Consumable's rarity
-     * @param {string} type The Consumable's type
      * @param {array} effects The Consumable's effects
      * @param {array} triggers The Consumable's triggers
      */
     constructor(name, desc, icon, price, rarity, props) {
         super(name, desc, icon, price, rarity);
-        this.type = getValueFromObject(props, 'type', Data.ConsumableType.EDIBLE);
         this.effects = getValueFromObject(props, 'effects', null);
         this.triggers = getValueFromObject(props, 'triggers', null);
         this.variables = getValueFromObject(props, 'variables', null);
