28c57ec4f40d44ad834e6aa809889bce3f3eaa57
nicolas prevot <nicolas1.prevot@orange.com>
2023-07-04 15:35:19 +0200

    added soulbinding item drag and drop feature

--- src/scripts/soulbinding.js
+++ src/scripts/soulbinding.js
@@ -2,4 +2,17 @@ class Soulbinding {
     constructor() {
         this.item = null;
     }
+
+    setItem(event) {
+        // retrieving data
+        const weapon = event.dataTransfer.getData("weapon");
+        const armor = event.dataTransfer.getData("armor");
+        const trinket = event.dataTransfer.getData("trinket");
+
+        if(weapon !== '') this.item = getInventoryWeaponById(Number(weapon));
+        else if(armor !== '') this.item = getInventoryArmorById(Number(armor));
+        else if(trinket !== '') this.item = getInventoryTrinketById(Number(trinket));
+
+        getSoulbindingItem(true);
+    }
 }
