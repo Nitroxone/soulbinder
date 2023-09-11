76a2a48a7abc67a4dd4124b5f16b495ac913617e
ntrx <kraenash@gmail.com>
2023-09-11 16:43:26 +0200

    improved slmrk progress display on sr

--- src/scripts/graphics.js
+++ src/scripts/graphics.js
@@ -2114,7 +2114,7 @@ function getFormattedSoulmark(sm, soulreadingFormat = false) {
 function getSoulmarkProgressGauge(sm) {
     let str = '';
 
-    str += '<div class="gaugeProgress"><div class="statGauge soulmark" style="width: ' + Math.round((sm.studied*100)/sm.researchTotal) + '%"><span class="gaugeIndicator">' + sm.studied + '/' + sm.researchTotal + '</span></div></div>';
+    str += '<div class="gaugeProgress slmrkGauge"><div class="statGauge soulmark" style="width: ' + Math.round((sm.studied*100)/sm.researchTotal) + '%"><span class="gaugeIndicator">' + sm.studied + '/' + sm.researchTotal + '</span></div></div>';
     str += '<div class="alchToxicity">';
     str += '<div class="slmrkStatus ' + (sm.unlocked ? 'un' : '') + 'locked"><div class="slmrkStatusIcon"></div>' + (sm.unlocked ? 'Unl' : 'L') + 'ocked</div>';
     str += '<div id="slmrkNum-' + sm.name + '" class="slmrkNum"></div>';
