2125068a792c2374522ab6e08c3c501ee13c7766
ntrx <kraenash@gmail.com>
2023-09-24 18:04:05 +0200

    added tooltip sound

--- src/scripts/events/ev_exploration.js
+++ src/scripts/events/ev_exploration.js
@@ -8,11 +8,13 @@ function generateExplorationHubEvents() {
         game.all_dungeons.filter(x => x.biome === bio.firstChild.textContent).forEach(dun => {
             dun.lootConfig?.resources?.forEach(re => {
                 addTooltip(domWhat('edls-' + re.id), function(){
+                    Sounds.Methods.playSound(Data.SoundType.TOOLTIP_HOVER);
                     return getResourceTooltip(re);
                 }, {offY: -8});
             });
             dun.lootConfig?.sets?.forEach(re => {
                 addTooltip(domWhat('edls-' + re.id), function(){
+                    Sounds.Methods.playSound(Data.SoundType.TOOLTIP_HOVER);
                     return getSetTooltip(re);
                 }, {offY: -8});
             });
