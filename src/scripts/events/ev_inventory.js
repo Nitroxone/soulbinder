c2b52198681862bbc30168de278723cd85479f74
ntrx <kraenash@gmail.com>
2023-09-27 21:37:05 +0200

    added tooltips and sounds to knapsack

--- src/scripts/events/ev_inventory.js
+++ src/scripts/events/ev_inventory.js
@@ -2,11 +2,31 @@ function generateDungeonKnapsackEvents() {
     const items = document.querySelectorAll('.knpsckContent .inventoryItem');
 
     items.forEach(it => {
+        const obj = game.player.du_inventory.find(x => x.id == it.id.slice(5));
+
+        addTooltip(it, function(){
+            if(obj instanceof Weapon) return getWeaponTooltip(obj);
+            if(obj instanceof Armor) return getArmorTooltip(obj);
+            if(obj instanceof Trinket) return getTrinketTooltip(obj);
+            if(obj instanceof Resource) return getResourceTooltip(obj);
+            if(obj instanceof Sigil) return getSigilTooltip(obj);
+            if(obj instanceof Consumable) return getConsumableTooltip(obj);
+        }, {offY: -8});
+
+        // Spawn tooltip and play sound on click
+        it.addEventListener('click', function(){
+            Sounds.Methods.playSound(Data.SoundType.TOOLTIP_SPAWN);
+            spawnTooltip(obj);
+        });
+        // Play sound on hover
+        it.addEventListener('mouseover', function(){
+            Sounds.Methods.playSound(Data.SoundType.TOOLTIP_HOVER);
+        });
+
         it.addEventListener('contextmenu', e => {
             e.stopImmediatePropagation();
             e.preventDefault();
 
-            obj = game.player.du_inventory.find(x => x.id == it.id.slice(5));
             game.player.removeFromKnapsack(obj);
         })
     })
