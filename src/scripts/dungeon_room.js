dba91166df8c346c296e8686b4fd3569eb597224
ntrx <kraenash@gmail.com>
2023-10-18 10:19:19 +0200

    adding missing dungeon_room doc

--- src/scripts/dungeon_room.js
+++ src/scripts/dungeon_room.js
@@ -27,6 +27,11 @@ class DungeonRoom {
         this.revealedCluster = false;
     }
 
+    /**
+     * Returns this room's description.
+     * @param {boolean} reassign whether to reassign the room's description
+     * @returns {string} the room's description
+     */
     getRoomDescription(reassign = false) {
         if(this.desc !== '' && !reassign) return this.desc;
         let desc = '';
@@ -41,6 +46,10 @@ class DungeonRoom {
         return desc;
     }
 
+    /**
+     * Returns whether the room's type is searchable.
+     * @returns {boolean} whether the room can be searched
+     */
     canSearch() {
         return this.type === Data.DungeonRoomType.ANTECHAMBER_OF_MARVELS 
                 || this.type === Data.DungeonRoomType.DESECRATED_ALTAR
@@ -49,6 +58,10 @@ class DungeonRoom {
                 || this.type === Data.DungeonRoomType.ETERNITY_WELL;
     }
 
+    /**
+     * Returns this room's actions.
+     * @returns {Data.DungeonRoomAction[]}
+     */
     getActions() {
         let actions = [];
         if(this.isCleared()) return actions;
@@ -61,6 +74,10 @@ class DungeonRoom {
         return actions;
     }
 
+    /**
+     * Scouts this room. Removes a Solar Firely from the player's inventory.
+     * @returns {boolean} whether the scout was successful (if the player has at least one Solar Firefly)
+     */
     scout() {
         if(hasResource(game.player.inventory.resources, 'solar firefly')) {
             this.identify();
@@ -69,20 +86,51 @@ class DungeonRoom {
         } else return false;
     }
 
+    /**
+     * Identifies the room and updates its description.
+     */
     identify() {
         this.identified = true;
         this.getRoomDescription(true);
     }
 
+    /**
+     * Changes the room's status to CLEARED.
+     */
     clear() {
         this.status = Data.DungeonRoomStatus.CLEARED;
     }
 
+    /**
+     * Returns whether this room is cleared.
+     * @returns {boolean} 
+     */
     isCleared() {
         return this.status === Data.DungeonRoomStatus.CLEARED;
     }
 
+    /**
+     * Changes the room's status to ENGAGED.
+     */
     engage() {
         this.status = Data.DungeonRoomStatus.ENGAGED;
+    }
+
+    /**
+     * Returns whether this room is a combat room.
+     * @returns {boolean}
+     */
+    isCombatRoom() {
+        return this.type === Data.DungeonRoomType.BOSS
+                || this.type === Data.DungeonRoomType.DORMANT_ROOM
+                || this.type === Data.DungeonRoomType.FRACTURED_HOLLOW
+                || this.type === Data.DungeonRoomType.SACRIFICIAL_ALCOVE
+    }
+
+    /**
+     * Enters the room.
+     */
+    enterRoom() {
+
     }
 }
