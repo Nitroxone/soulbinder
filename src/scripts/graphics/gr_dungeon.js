8836d67442e83592079169ca6c6b1302f8f2ba52
ntrx <kraenash@gmail.com>
2023-11-15 16:49:22 +0100

    fixed looted room item display

--- src/scripts/graphics/gr_dungeon.js
+++ src/scripts/graphics/gr_dungeon.js
@@ -111,7 +111,7 @@ function drawDungeonFoundLoot(refresh = false) {
         let timer = 0;
         str += '<div class="roomLootResult-title">Loot found</div>';
         loot.forEach(lo => {
-            str += '<div class="roomLootResult-listItem sigilInfo revealingLoot' + (lo.looted ? ' lootedLoot' : '') + '" style="animation-delay: ' + timer + 's;' + (lo.type === 'gold' ? 'background-image: url(\'css/img/goldicon.png\'); background-size: 25%;' : getIcon(lo.item, 25, true)) + '">';
+            str += '<div id="loit-' + (lo.type === 'gold' ? 'gold' : lo.item.id) + '" class="roomLootResult-listItem sigilInfo revealingLoot' + (lo.looted ? ' lootedLoot' : '') + '" style="animation-delay: ' + timer + 's;' + (lo.type === 'gold' ? 'background-image: url(\'css/img/goldicon.png\'); background-size: 25%;' : getIcon(lo.item, 25, true)) + '">';
             str += '<div class="sigilTitle" style="text-align: left">' + '<span class="lootQuantity">' + lo.amount + ' </span>' + (lo.type === 'gold' ? '<span class="smallThingNoIcon" style="color: yellow">Gold</span>' : getSmallThingNoIcon(lo.item, null)) + '</div>';
             str += '<div class="revealingLootAnim revealLoot' + (lo.type === 'gold' ? 'Gold' : capitalizeFirstLetter(lo.item.rarity)) + '" style="animation-delay: ' + (timer) + 's;"></div>';
             if(refresh) str += '<canvas class="revealingLootCanvas ' + (lo.type === 'gold' ? 'gold' : lo.item.rarity) + '"></canvas>';
