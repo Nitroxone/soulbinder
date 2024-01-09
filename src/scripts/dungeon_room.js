2eb1d5c8ce0cd79e6c9ebc8158cb77d672fbd407
ntrx <kraenash@gmail.com>
2024-01-09 15:17:26 +0100

    dust off useless dungeon room attributes

--- src/scripts/dungeon_room.js
+++ src/scripts/dungeon_room.js
@@ -9,11 +9,8 @@ class DungeonRoom {
         this.id = Math.floor(Math.random() * Date.now());
         this.coordinates = getValueFromObject(props, "coordinates", [0, 0]);
         this.type = getValueFromObject(props, "type", Data.DungeonRoomType.EMPTY);
-        this.childrenRooms = getValueFromObject(props, "childrenRooms", []);
-        this.clusterBridge = getValueFromObject(props, "clusterBridge", null);
         this.nextRoom = getValueFromObject(props, "nextRoom", null);
         this.previousRoom = getValueFromObject(props, "previousRoom", null);
-        this.parentCluster = getValueFromObject(props, "parentCluster", null);
 
         this.status = Data.DungeonRoomStatus.UNCLEARED;
         this.visited = false;
@@ -25,8 +22,6 @@ class DungeonRoom {
         this.foundLoot = null;
 
         this.enemyGroup = null;
-
-        this.revealedCluster = false;
     }
 
     /**
