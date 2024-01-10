8dee35bd5028734c212de786d574f10f341e532e
ntrx <kraenash@gmail.com>
2024-01-10 11:53:49 +0100

    re-switch to preloaded dungeon

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -382,8 +382,8 @@ class Game {
         //this.inventory.enchant(this.inventory.weapons[0], this.inventory.sigils[1]);
         //this.inventory.enchant(this.inventory.armors[0], this.inventory.sigils[0]);
 
-        //this.currentDungeon = this.all_dungeons.find(x => x.name === 'Putrescent Ossuary');
-        //this.currentDungeon.init();
+        this.currentDungeon = this.all_dungeons.find(x => x.name === 'Putrescent Ossuary');
+        this.currentDungeon.init();
 
         // Config.Soulwriting.forEach(sw => {
         //     sw.unlocked = true;
@@ -398,8 +398,8 @@ class Game {
         drawStridersScreen();
         drawHubScreen();
         drawWorkshopScreen();
-        //drawExplorationScreen();
-        drawExplorationHubScreen();
+        drawExplorationScreen();
+        //drawExplorationHubScreen();
         drawEonScreen();
         drawEmptyBattleScreen();
 
